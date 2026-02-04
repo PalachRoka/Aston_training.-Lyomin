dfa34389cd4a0474e77d9aa3fa4e6202e32cb1d3
PalachRoka <100845778+PalachRoka@users.noreply.github.com>
2026-02-04 18:04:03 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+    }}
+
+class Park {
+    private String parkName;
+    Park.Attraction attraction = new Park().new Attraction();
+
+private class Attraction {
+    private String nameAttraction;
+    private String workTime;
+    private int price;
+
+}}
